f115845cb10293650115fdbdda73ae85b3888a4b
Vipul Chaudhary <77090657+vipulchaudhary16@users.noreply.github.com>
2023-02-23 22:28:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ const MONGO_URI = process.env.MONGO_URI;
 connectToDatabase = () => {
     mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
     }).then(() => {
         console.log("Connection Successful");
     }).catch((e) => {
